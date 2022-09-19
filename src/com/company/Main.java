707c20da19518bed7b794eaa484f6854ee6562e2
hooda.armaan <armaan.hooda040406@gmail.com>
2022-09-19 19:23:32 +0300

    Secondary commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,13 @@ public class Main
 {
     public static void main(String[] args)
     {
+        Method_1(args);
+        Method_2(args);
+    }
+
+    static void Method_1(String[] args)
+    {
+
         //taking input
         String firstInput = args[0];
         String secondInput = args[1];
@@ -16,7 +23,7 @@ public class Main
 
         //finding processed output
         int firstAppearance = firstInput.indexOf(secondInput);
-        if (firstAppearance!=-1)
+        if (firstAppearance != -1)
             if (!firstInput.substring(0, firstAppearance).equals(""))
                 answer.append(firstInput, 0, firstAppearance).append(" ");
 
@@ -46,5 +53,53 @@ public class Main
 
         //printing final answer
         System.out.println(answer);
+    }
+
+    static void Method_2(String[] args)
+    {
+        //Method 2
+
+        String one = args[0];
+        String two = args[1];
+
+        //making a mutable StringBuilder
+        StringBuilder output = new StringBuilder();
+
+        //adding the string to the StringBuilder
+        output.append(one);
+
+        //checking for required subString
+        boolean contains = true;
+        while (contains)
+        {
+            if (output.toString().contains(two))
+            {
+                int position = output.indexOf(two);
+                output.delete(position, position + 2);
+                output.insert(position, " ");
+            }
+            else
+                contains = false;
+        }
+
+        //emptying out the extra white spaces
+        for (int a = 0; a < output.length(); a++)
+        {
+            int n = 1;
+            while (n > 0)
+            {
+                if (output.charAt(a) == ' ' && output.charAt(a + n) == ' ')
+                {
+                    output.delete(a, a + n);
+                }
+                else
+                    break;
+                n++;
+            }
+        }
+
+        //printing out final output
+        String final_answer = output.toString().trim();
+        System.out.println(final_answer);
     }
 }
